2410b9c5b058190d71cfde0251bd88bfcf178819
Sjain-dir <saksham.jain.work@gmail.com>
2023-03-07 22:19:56 +0530

    making structs and implimenting basic functions testing pending...

--- libinterface/interface.c
+++ libinterface/interface.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include <arpa/inet.h>  
+#include <stdio.h>  
+#include <string.h>  
+#include <sys/socket.h>  
+#include <unistd.h>  
 
-char test (int input) {
-    printf("this is your number %d \n", input);
-    return 'a';
-}
+// char test (int input) {
+//     printf("this is your number %d \n", input);
+//     return 'a';
+// }
+
+extern struct sockaddr_in;
+
+extern int socket(int domain, int type, int protocol);
+
+extern uint16_t htons (uint16_t __hostshort)
+     __THROW __attribute__ ((__const__));
+
+extern int inet_pton (int __af, const char *__restrict __cp,
+		      void *__restrict __buf) __THROW;
+
+extern int connect(int __fd, const struct sockaddr *__addr, socklen_t __len);
+
+//extern size_t strlen(const char *__s); // can use the rust inbuilt function.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
-use std::os::raw::{
+use std::ffi::{
         c_char, 
-        c_int
+        c_int,
+        c_uint,
+        c_void
     };
-use std::char;
 
+
+
+// struct sockaddr_in {
+//     sa_family_t    sin_family; /* address family: AF_INET */
+//     in_port_t      sin_port;   /* port in network byte order */
+//     struct in_addr sin_addr;   /* internet address */
+// };
+//
+// The <sys/socket.h> header shall define the unsigned integer type sa_family_t.
+// typedef uint16_t in_port_t;
+// struct in_addr {
+//     uint32_t       s_addr;     /* address in network byte order */
+// };
+
+struct in_addr {
+    s_addr : u32,
+}
+
+struct sockaddr_in{
+    sin_family : c_uint,
+    sin_port   : u16,
+    sin_addr   : in_addr,
+}
 
 #[link (name = "interface", kind = "static")]
 extern "C" {
-    fn test(input : c_int) -> c_char;
+    // fn test(input : c_int) -> c_char;
+    fn socket(domain: c_int, type1 : c_int,protocol : c_int) -> c_int;
+    fn htons (__hostshort : u16) -> u16;
+    fn inet_pton (__af : c_int, __cp : *mut c_char, __buf : *mut c_void) -> c_int;
+    fn connect(__fd : c_int, __addr : *mut sockaddr_in, __len: c_uint) -> c_int;
+
 }
 
 fn main() {
-    let mut output : i8;
-    unsafe {
-        output = test(5);
-    };
-    let output = (output as u8 ) as char;
-    println!("Hello, world! {:?}", output.to_string());
+    // let mut output : i8;
+    // unsafe {
+    //     output = test(5);
+    // };
+    // let output = (output as u8 ) as char;
+    // println!("Hello, world! {:?}", output.to_string());
 }
